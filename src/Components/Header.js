5435521f5a1c3afae55451555620e234e98e4660
Jasurbek <jasurisoqov685@gmail.com>
2022-06-19 22:20:07 +0500

    shooping icon link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div className="ui pointing menu" style={{ padding: "5px 10px" }}>
-      <a className="active item">Shopping</a>
+      <Link to="/">
+        <a className="active item">Shopping</a>
+      </Link>
       <div className="right menu">
         <div className="ui vertical animated button" tabIndex="0">
           <div className="hidden content">Shop</div>
