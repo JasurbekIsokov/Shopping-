a606c1e1ed0aeebe101defc12b15173d54537c3b
Jasurbek <jasurisoqov685@gmail.com>
2022-06-19 21:57:35 +0500

    productDetail

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -8,6 +8,7 @@ import { selectProduct } from "../Services/Actions/Actions";
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
+  const { image, title, price, category, description } = product;
   const { productId } = useParams();
   const dispatch = useDispatch();
   // console.log(productId);
@@ -25,14 +26,41 @@ const ProductDetails = () => {
   };
 
   useEffect(() => {
-    axiosProductDetail();
-  }, []);
+    if (productId && productId !== "") axiosProductDetail();
+  }, [productId]);
 
   return (
     <div>
       <Header />
-      <div>
-        <h1>ProductDetails</h1>
+      <div className="ui grid container">
+        {Object.keys(product).length === 0 ? (
+          <div>...loading</div>
+        ) : (
+          <div className="ui placeholder segment">
+            <div className="ui two column stackable center aligned grid">
+              <div className="ui vertical divider">AND</div>
+              <div className="middle aligned row">
+                <div className="column lp">
+                  <img src={image} alt={title} />
+                </div>
+                <div className="column rp">
+                  <h1>{title}</h1>
+                  <h2>
+                    <a className="ui teal tag label">${price}</a>
+                  </h2>
+                  <h3 className="ui brown block header">{category}</h3>
+                  <p>{description}</p>
+                  <div className="ui vertical animated button" tabIndex="0">
+                    <div className="hidden content">
+                      <i className="shop icon"></i>
+                    </div>
+                    <div className="visible content">Add To Card</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
